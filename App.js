e8903d90961519d6fb4defb5c1fadfca745427b8
Aathif26 <aathif0710@gmail.com>
2024-09-20 12:20:07 +0530

    Bottom Tabs Added

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Home from "./src/pages/Home";
+import Explore from "./src/pages/Explore";
+import { Entypo, MaterialIcons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+  tabBarShownLabel: false,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    background: "#fff",
+  },
+};
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-300">
-      <Text className="text-2xl font-semibold">Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View className="align-middle justify-center">
+                  <Entypo
+                    name="home"
+                    size={24}
+                    color={focused ? "#16247d" : "#111"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Explore"
+          component={Explore}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View className="align-middle justify-center">
+                  <MaterialIcons
+                    name="explore"
+                    size={24}
+                    color={focused ? "#16247d" : "#111"}
+                  />
+                </View>
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/pages/Explore.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Explore() {
+    return (
+        <View className="flex-1 items-center justify-center bg-gray-300">
+            <Text className="text-xl font-bold">Explore</Text>
+        </View>
+    )
+}
